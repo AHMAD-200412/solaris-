9d2235377417e88994dfc484ec707352841a0a8c
AHMAD-200412 <ahmad2004asa@gmail.com>
2026-07-23 00:39:05 +0300

    Update settings

--- simulator/views.py
+++ simulator/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
 
+from django.shortcuts import render, redirect
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    if request.user.is_authenticated:
+        if request.user.user_type == "company":
+            return redirect("/dashboard/company/")
+        return redirect("/user/home/")
+
+    return render(request, "home.html")
